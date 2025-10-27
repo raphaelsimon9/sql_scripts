8029bc07da1bee8c4c2c224853314796a05becbe
Raphael Simon <olasunkanmi9190@gmail.com>
2025-10-27 11:57:06 +0100

    Update Challenges.sql

--- Challenges.sql
+++ Challenges.sql
@@ -42,3 +42,42 @@ from(
 	) as bottom_customers
 order by `Number of Orders` desc;
 
+-- Get the customer names, the number of orders and the full names of employees that took care of the orders
+select orderID, companyName, concat_ws(' ', e.firstname, e.lastname) as `Employee Name`
+from customers c join orders o
+using(customerID)
+join employees e
+using(employeeID);
+
+-- Get the product name, category, and the suppliers of each product
+describe products;
+describe suppliers;
+describe categories;
+select productName `Product Name`, categoryName `Product Category`, companyName `Supplier Name`
+from suppliers join products
+using(supplierID)
+join categories
+using(categoryID);
+
+-- Get the Order ID, Customer's Name, Grand Total of each Order, Name of Employee
+show tables;
+describe orderdetails;
+describe employees;
+describe customers;
+describe orders;
+
+select * from orderdetails
+limit 5;
+select o.orderID as `Order ID`,
+companyName as `Customer Name`,
+concat('$ ',format(sum(UnitPrice * Quantity),2)) as `Grand Total`,
+concat_ws(' ', firstName, lastName) as `Employee Name`
+from employees
+join orders o
+using(employeeID)
+join customers
+using(customerID)
+join orderdetails od
+on o.orderID = od.orderID
+group by od.orderID
+order by o.orderID asc;
